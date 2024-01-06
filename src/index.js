6762940781c05833a2f6335963c1c8cde725e634
YukiGasai <r.lindede@googlemail.com>
2024-01-06 04:03:46 +0100

    Add back Strict mode

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 
-// ReactDOM.createRoot(document.getElementById("root"))?.render(
-//   <React.StrictMode>
-//       <App />
-//   </React.StrictMode>
-// );
-
 ReactDOM.createRoot(document.getElementById("root"))?.render(
+  <React.StrictMode>
       <App />
+  </React.StrictMode>
 );
+
+// ReactDOM.createRoot(document.getElementById("root"))?.render(
+//       <App />
+// );
